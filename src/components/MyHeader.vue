d8486eb641b8e4980eef9f3108eb6ab12ffd4e81
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-26 15:45:45 +0100

    costruzione header

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <p>&#9679;</p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header {
+  height: 100px;
+  background-color: #2e3a46;
+  position: relative;
+
+  p {
+    color: #1ed55f;
+    font-size: 80px;
+    position: absolute;
+    top: 10px;
+    left: 10px;
+  }
+}
+</style>
